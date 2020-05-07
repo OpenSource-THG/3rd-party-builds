646ee217b38c5eec744e2b3d5b560082d9c2bcf0
Josh Blease <josh.blease@thehutgroup.com>
2020-05-07 10:00:01 +0100

    correct variable name in upload script

--- haproxy/docker/s3upload.sh
+++ haproxy/docker/s3upload.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws s3 cp /build/haproxy-2.0.14/haproxy s3://$1/2.0.14/haproxy --profile "${1:-default}"
+aws s3 cp /build/haproxy-2.0.14/haproxy s3://$1/2.0.14/haproxy --profile "${2:-default}"
